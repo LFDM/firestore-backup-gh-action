430f69b64dbc0888dd8b04d8b78800b6ddc704a0
LFDM <1986gh@gmail.com>
2019-09-04 10:38:29 -0700

    Implement entry point

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+
+#!/bin/sh
+
+set -e
+
+SERVICE_ACCOUNT_FILE="$HOME/firebase_backup_gcloud_service_account.json"
+echo "$INPUT_GCLOUDAUTH" | base64 --decode > "$SERVICE_ACCOUNT_FILE"
+sh -c "gcloud auth activate-service-account --key-file=$SERVICE_ACCOUNT_FILE"
+rm "$SERVICE_ACCOUNT_FILE"
+
+if [[ -z $INPUT_COLLECTIONIDS]] then
+  COLLECTION_IDS="--collection-ids $INPUT_COLLECTIONIDS"
+fi
+
+sh -c "gcloud beta firestore --project=$INPUT_PROJECTID" $INPUT_STORAGEBUCKET $COLLECTION_IDS"
