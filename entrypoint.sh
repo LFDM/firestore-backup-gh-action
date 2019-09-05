e82ddb75d6f703a4665dc2eb1157f2675032de8e
LFDM <1986gh@gmail.com>
2019-09-04 21:27:36 -0700

    Move --quiet to the right place

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 
 SERVICE_ACCOUNT_FILE="$HOME/firebase_backup_gcloud_service_account.json"
 echo "$INPUT_GCLOUDAUTH" | base64 --decode > "$SERVICE_ACCOUNT_FILE"
-sh -c "gcloud auth activate-service-account --key-file=$SERVICE_ACCOUNT_FILE --quiet"
+sh -c "gcloud auth --quiet activate-service-account --key-file=$SERVICE_ACCOUNT_FILE"
 rm "$SERVICE_ACCOUNT_FILE"
 
 [ -z "$INPUT_COLLECTIONIDS" ] || COLLECTION_IDS="--collection-ids $INPUT_COLLECTIONIDS"
 
-sh -c "gcloud beta firestore --project=$INPUT_PROJECTID export $INPUT_STORAGEBUCKET $COLLECTION_IDS --quiet"
+sh -c "gcloud beta firestore --quiet --project=$INPUT_PROJECTID export $INPUT_STORAGEBUCKET $COLLECTION_IDS"
