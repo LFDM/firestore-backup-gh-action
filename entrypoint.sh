b2facd500f8236b2a6e16f7ade00641e66da4c94
LFDM <1986gh@gmail.com>
2019-09-04 18:50:12 -0700

    Fix syntax errors in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,6 @@ echo "$INPUT_GCLOUDAUTH" | base64 --decode > "$SERVICE_ACCOUNT_FILE"
 sh -c "gcloud auth activate-service-account --key-file=$SERVICE_ACCOUNT_FILE"
 rm "$SERVICE_ACCOUNT_FILE"
 
-if [[ -z $INPUT_COLLECTIONIDS]] then
-  COLLECTION_IDS="--collection-ids $INPUT_COLLECTIONIDS"
-fi
+[ -z "$INPUT_COLLECTIONIDS" ] || COLLECTION_IDS="--collection-ids $INPUT_COLLECTIONIDS"
 
-sh -c "gcloud beta firestore --project=$INPUT_PROJECTID" $INPUT_STORAGEBUCKET $COLLECTION_IDS"
+sh -c "gcloud beta firestore --project=$INPUT_PROJECTID export $INPUT_STORAGEBUCKET $COLLECTION_IDS"
