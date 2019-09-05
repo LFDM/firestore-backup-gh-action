a5b21f36f1fe33210474e9d546fadada57e60a12
LFDM <1986gh@gmail.com>
2019-09-04 21:33:33 -0700

    Try to install beta components manually

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,11 @@ set -e
 
 SERVICE_ACCOUNT_FILE="$HOME/firebase_backup_gcloud_service_account.json"
 echo "$INPUT_GCLOUDAUTH" | base64 --decode > "$SERVICE_ACCOUNT_FILE"
-sh -c "gcloud auth --quiet activate-service-account --key-file=$SERVICE_ACCOUNT_FILE"
+sh -c "gcloud auth activate-service-account --key-file=$SERVICE_ACCOUNT_FILE"
 rm "$SERVICE_ACCOUNT_FILE"
 
 [ -z "$INPUT_COLLECTIONIDS" ] || COLLECTION_IDS="--collection-ids $INPUT_COLLECTIONIDS"
 
-sh -c "gcloud beta firestore --quiet --project=$INPUT_PROJECTID export $INPUT_STORAGEBUCKET $COLLECTION_IDS"
+
+sh -c "gcloud components install beta"
+sh -c "gcloud beta firestore --project=$INPUT_PROJECTID export $INPUT_STORAGEBUCKET $COLLECTION_IDS"
